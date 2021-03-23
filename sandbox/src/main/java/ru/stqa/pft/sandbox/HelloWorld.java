245e6e6b75001227310b3f062ec9327204ddc46a
AnaHuana <80969959+AnaHuana@users.noreply.github.com>
2021-03-23 13:31:39 +0300

    вычисления

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -4,5 +4,12 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(1/2);
+        System.out.println(1.0/2);//для выполнения вычислений не в целых числах
+        System.out.println("2" + "3");// Конкатенация
+        System.out.println("2 + 2 ="+ (2+2));
     }
 }
